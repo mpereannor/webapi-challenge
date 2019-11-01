107968af9831145a459a817b9786c67c2f950adf
chudnofskyATD <mpereannor@gmail.com>
2019-11-01 07:22:34 +0000

    project router set up

---
+++ data/routers/projects.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const ProjectsDb = require('../helpers/projectModel');
+const ActionsDb = require('../helpers/actionModel');
+
+const router = express.Router();
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
-//const actionRouter = require('./data/routers/actions');
-//const projectsRouter = require('./data/routers/projects');
+const actionRouter = require('./data/routers/actions');
+const projectsRouter = require('./data/routers/projects');
 const app = express();
 
 app.use(helmet());
+app.use(logger);
 app.use(express.json());
-//app.use('/api/actions', actionRouter);
-//app.use('/api/projects', projectsRouter);
+app.use('/api/actions', actionRouter);
+app.use('/api/projects', projectsRouter);
 
 app.get('/', (req, res) => {
     res.send(`<h2 spring challenge</h2>`)
 });
+
+//custom logger middleware
+function logger(req, res, next) { 
+    console.log(req.method,
+                req.url,
+                new Date()
+                .toLocaleTimeString());
+                next();
+}
 
 module.exports = app
