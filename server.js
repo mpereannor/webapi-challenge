81c195608d6016343ce6d426f1f3d46a0428e027
chudnofskyATD <mpereannor@gmail.com>
2019-11-01 16:31:52 +0000

    self study questions answered

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const projectsRouter = require('./data/routers/projects');
 const app = express();
 
 app.use(helmet());
-//app.use(logger);
 app.use(express.json());
 app.use('/api/actions', actionRouter);
 app.use('/api/projects', projectsRouter);
@@ -13,14 +12,5 @@ app.use('/api/projects', projectsRouter);
 app.get('/', (req, res) => {
     res.send(`<h2 spring challenge</h2>`)
 });
-
-//custom logger middleware
-// function logger(req, res, next) { 
-//     console.log(req.method,
-//                 req.url,
-//                 new Date()
-//                 .toLocaleTimeString());
-//                 next();
-// }
 
 module.exports = app
