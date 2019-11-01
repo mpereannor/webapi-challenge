bd6369d88ced39b802e73df4081377bd22df9d89
chudnofskyATD <mpereannor@gmail.com>
2019-11-01 07:13:04 +0000

    first commit, api tested

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+const server = require('./server');
+//const port = 7000;
+server.listen(7000, () => {
+    console.log(
+        `\n* Server Running on http://localhost:7000 *\n`
+    )
+});
